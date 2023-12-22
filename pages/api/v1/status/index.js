6d4c24b228df7ea27496a10005ebdab0b0a208bb
Gustavo Keller <85264199+eugustavokeller@users.noreply.github.com>
2023-12-22 14:23:55 +0000

    alter return endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT NOW();");
-  console.log(result);
-  response.status(200).json({ chave: "VALOR" });
+  const updatedAt = new Date().toISOString();
+
+  const resultDbMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(resultDbMaxConnections[0].max_connections);
+
+  const dbName = process.env.POSTGRES_DB;
+  const resultDbAtiveConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const activeConnections = resultDbAtiveConnections[0].count;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: maxConnections,
+        opened_connections: activeConnections,
+      },
+    },
+  });
 }
 
 export default status;
